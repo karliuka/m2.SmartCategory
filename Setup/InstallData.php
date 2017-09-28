9fd5226b41c7eb5e0a253f12b1ece0666b472bab
karliuka <karliuka.vitalii@gmail.com>
2017-09-28 08:44:10 +0300

    copyright

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -1,23 +1,8 @@
 <?php
 /**
- * Faonni
- *  
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- *
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade module to newer
- * versions in the future.
+ * Copyright © 2011-2017 Karliuka Vitalii(karliuka.vitalii@gmail.com)
  * 
- * @package     Faonni_SmartCategory
- * @copyright   Copyright (c) 2017 Karliuka Vitalii(karliuka.vitalii@gmail.com) 
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * See COPYING.txt for license details.
  */
 namespace Faonni\SmartCategory\Setup;
 
