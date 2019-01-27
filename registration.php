b02b5212c5333779513195ba88ebfea569ec2322
karliuka <karliuka.vitalii@gmail.com>
2019-01-27 23:18:09 +0200

    copyright

--- registration.php
+++ registration.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Copyright © 2011-2018 Karliuka Vitalii(karliuka.vitalii@gmail.com)
- *
+ * Copyright © Karliuka Vitalii(karliuka.vitalii@gmail.com)
  * See COPYING.txt for license details.
  */
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+ use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
     'Faonni_SmartCategory',
     __DIR__
 );
